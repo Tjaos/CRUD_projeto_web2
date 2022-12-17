543a9ae6fa00d7bf30ddba35c4bad2adfd48d38c
Thiago Amaral <92883620+Tjaos@users.noreply.github.com>
2022-12-17 20:55:39 -0300

    Add files via upload
    Mudança no projeto, agora usa-se o express.

---
+++ minhaApi.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const port = 8081;
+const fs = require("fs");
+const bodyParser = require("body-parser");
+let encodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get("/", (req, res) => {
+  res.send("<p> Teste</p>");
+});
+app.get("/anunciaItem", (req, res) => {
+  let form = "<form action='/anuncios'method='POST'>";
+  form += "<label>Tipo: </label><input type:'text' name='tipo'><br>";
+  form += "<label>Produto: </label><input type:'text' name='produto'><br>";
+  form += "<label>Descricao: </label><input type:'text' name='descricao'><br>";
+  form += "<label>Preco: </label><input type:'text' name='preco'><br>";
+  form += "<button>Enviar</button>";
+
+  res.send("<div>Anuncie aqui</div><br>" + form);
+});
+
+app.get("/buscaItem", (req, res) => {
+  let form = "<form action='/anuncios' method='GET'>";
+  form += "<label>Tipo: </label><input type='text' name='tipo'><br>";
+  form += "<label>Produto: </label><input type='text' name='produto'><br>";
+  form += "<label>Descrição: </label><input type='text' name='descricao'><br>";
+  form += "<label>Preço: </label><input type='text' name='preco'><br>";
+  form += "<button>Buscar</button></form>";
+
+  res.send("<div>Procure aqui!</div><br>" + form);
+});
+
+app.post("/anuncios", encodedParser, (req, res) => {
+  let produto = req.body.produto;
+  let tipo = req.body.tipo;
+  let descricao = req.body.descricao;
+  let preco = req.body.preco;
+  let novoAnuncio = {
+    produto: produto,
+    tipo: tipo,
+    descricao: descricao,
+    preco: preco,
+  };
+
+  fs.readFile("meuBD.json", "utf-8", (erro, texto) => {
+    if (erro) throw "erro" + erro;
+    let meuBD = JSON.parse(texto);
+    meuBD.anuncios.push(novoAnuncio);
+    let meuBDJson = JSON.stringify(meuBD);
+    console.log(meuBDJson);
+
+    fs.writeFile("meuBD.json", meuBDJson, (erro) => {
+      if (erro) {
+        throw "erro:" + erro;
+      } else {
+        res.send(meuBDJson);
+      }
+    });
+  });
+});
+
+app.get("/anuncios", (req, res) => {
+  let tipo = req.query.tipo;
+  let produto = req.query.produto;
+  let preco = req.query.preco;
+
+  console.log(req.query);
+
+  fs.readFile("meuBD.json", "utf8", (erro, texto) => {
+    if (erro) throw "Deu algum erro: " + erro;
+    let meuBD = JSON.parse(texto);
+    let anuncios = meuBD.anuncios;
+    let filtrado = anuncios.filter(
+      (valor) =>
+        parseFloat(valor.preco) < preco &&
+        valor.tipo.toLowerCase().includes(tipo.toLowerCase()) &&
+        valor.produto.toLowerCase().includes(produto.toLocaleLowerCase())
+    );
+    let resultado = "";
+
+    for (let i = 0; i < filtrado.length; i++) {
+      resultado += "<a href='/detalhe/" + filtrado[i].tipo + "'>";
+      resultado += `<b>Produto: </b> ${filtrado[i].produto}`;
+      resultado += `<b>Descrição: </b> ${filtrado[i].descricao}`;
+      resultado += `<b>Preço: </b> ${filtrado[i].preco}`;
+      resultado += "</a><br>";
+    }
+
+    res.send(filtrado);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Esta aplicação está escutando a
+	porta ${port}`);
+});
